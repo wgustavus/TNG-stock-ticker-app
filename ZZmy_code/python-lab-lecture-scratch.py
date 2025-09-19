58cca3c471dacbc23be9f786638337edcbaa507b
Wayne Gustavus <wgustavus@gmail.com>
2025-09-19 15:35:23 -0400

    Delete ZZmy_code directory
    clean up directory

--- ZZmy_code/python-lab-lecture-scratch.py
+++
@@ -1,9 +0,0 @@
-# Sep 2025 - Zach Wilson DataExpert BEGINNER Bootcamp
-# Python 'intro' video -- builds stock ticker via API script
-# NOTES: 
-#   - Requires Polygon.AI free account (https://polygon.io/)
-#   - Best practice = store API key in .env file
-#   - Best practice = store required libraries in a requirements.txt file (use to install: pip install -r requirements.txt)
-#
-#
-print('hello world')
